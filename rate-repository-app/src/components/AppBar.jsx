ba66cbae9b5b3d7602c2f15c78708fc263cb8e2c
ragul2k23 <rockyragulsiva2005@gmail.com>
2024-06-18 19:58:06 +0530

    done 10.9

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -2,18 +2,18 @@ import { View, StyleSheet, Pressable, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
 import { Link } from "react-router-native";
- 
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-      display:'flex',flexDirection:'row',
-      backgroundColor:'#e1e4e8'
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#e1e4e8",
   },
   text: {
     color: "white",
     padding: 10,
     backgroundColor: "#000800",
-  
   },
   // ...
 });
@@ -21,23 +21,22 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <ScrollView horizontal  >
-      <CreateLink to={'/'} text={"Repositories"} />
-      <CreateLink to={'signIn'} text={"Sign-In"} />
-
+      <ScrollView horizontal>
+        <CreateLink to={"/"} text={"Repositories"} />
+        <CreateLink to={"signIn"} text={"Sign-In"} />
       </ScrollView>
     </View>
   );
 };
-const CreateLink = ({to,text}) => {
-  return ( 
-    <Pressable >
+const CreateLink = ({ to, text }) => {
+  return (
+    <Pressable>
       <Link to={to}>
-        <Text style={styles.text} fontWeight="normal" fontSize="heading">
+        <Text style={styles.text} fontWeight="normal" fontSize="subheading">
           {text}
         </Text>
       </Link>
-    </Pressable> 
+    </Pressable>
   );
 };
 export default AppBar;
--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,97 @@
+import { useFormik } from "formik";
 import Text from "./Text";
+import { Pressable, StyleSheet, TextInput, View } from "react-native";
+import * as yup from "yup";
 
+const styles = StyleSheet.create({
+  errorText:{
+    color:"#d73a4a",
+    fontSize:17,
+    fontWeight:'400'
+  },
+  container: {
+    padding: 20,
+    borderWidth: 1,
+    borderBlockColor: "gray",
+  },
+  input: {
+    padding: 10,
+    fontSize: 18,
+    borderWidth: 1.3322,
+    marginVertical: 10,
+  },
+  submitBtn: {
+    color: "white",
+    padding: 6,
+    borderRadius: 7,
+    textAlign: "center",
+    marginTop:8
+  },
+});
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(6, "minimum 6 characters")
+    .max(8, 'Password cannot be more than 8 characters')
+    .required("username is required"),
+  password: yup
+    .string()
+    .min(6, "minimum 6 characters")
+    .max(8, 'Password cannot be more than 8 characters')
+    .required("password is required"),
+});
+const initialValues = {
+  username: "",
+  password: "",
+};
+const SignInForm = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+  return (
+    <View style={styles.container}>
+      <SingInTextInput formik={formik} field={"username"} />
+      <SingInTextInput formik={formik} field={"password"} />
+      <Pressable onPress={formik.handleSubmit}>
+        <Text
+          style={styles.submitBtn}
+          backgroundColor={"primary"}
+          fontSize={"heading"}
+        >
+          Submit
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+const SingInTextInput = ({formik,field}) =>{
+  const errorStyle = [styles.input,
+    formik.touched[field] && formik.errors[field]   && 
+   { borderColor:"red"}
+
+  ]
+  return( 
+    <>
+    <TextInput
+    style={errorStyle}
+    placeholder={field}
+    value={formik.values[field]}
+    onChangeText={formik.handleChange(field)}
+    maxLength={8}
+  />
+  {formik.touched[field] && formik.errors[field] && 
+       <Text style={styles.errorText}>{formik.errors[field]}</Text>
+      }
+  </>
+  )
+}
+const onSubmit = (values) => {
+  console.log(values.username, values.password);
+};
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  return <SignInForm onSubmit={onSubmit} />;
 };
 
 export default SignIn;
