204671799018aa426151ab093dcbb364af4eb677
ragul2k23 <rockyragulsiva2005@gmail.com>
2024-07-08 21:56:07 +0530

    update icon

--- rate-repository-app/app.config.js
+++ rate-repository-app/app.config.js
@@ -6,8 +6,18 @@ export default {
     "name": "rate-repository-app-rocky",
     extra: {
       env: process.env.ENV,
-      APOLLO_URI:process.env.APOLLO_URI
+      APOLLO_URI:process.env.APOLLO_URI,
+      "eas": {
+        "projectId": "e5f9b321-716e-4046-9d8e-3405451d7863"
+      }
     },
+  
+"platforms": [
+  "ios",
+  "android",
+  "web"
+],
+
     "slug": "rate-repository-app",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -28,10 +38,14 @@ export default {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
-      }
+      },
+      "package": "com.rocky.ragul",
+      "versionCode": 1
+
     },
     "web": {
       "favicon": "./assets/favicon.png"
-    }
+    },
+     
   }
 }
